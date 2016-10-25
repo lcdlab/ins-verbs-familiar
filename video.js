ac8dff7f17d82c4bb26e7452502224b4c32d1951
hughrabagliati <hugh.rabagliati@gmail.com>
2016-10-25 21:32:15 +0100

    adjust timing12

--- video.js
+++ video.js
@@ -485,7 +485,7 @@ function run_all_trials() {
 			t.fadeIn();
 			f.fadeIn();
 		}, 200);//current_trial['question_length']);//video.duration*1000  + 500);
-		console.log(video.currentTime);
+		console.log(video.currentTime > 0);
 		//setTimeout(close_fullscreen, video.duration*1000 - current_trial['question_length']);
 	})
 	// This will drop the video out of fullscreen in time for the question at the end.
