ea48e453c86bb62bc1f403a00ea3ebd375f83227
hughrabagliati <hugh.rabagliati@gmail.com>
2016-10-29 22:50:44 +0100

    check19

--- video.js
+++ video.js
@@ -449,7 +449,7 @@ function close_fullscreen() {
 
 function run_all_trials() {
 	$('#left').fadeOut(0);//.css("z-index", "99");
-	$('#right').fadeIn(0);//.css("z-index", "99");	
+	$('#right').fadeOut(0);//.css("z-index", "99");	
 	counterbalance_list = set_up_pseudo_counterbalance();
 	set_up_trial_list();
 	// If you run these the other way round, then trial list is edited in place and gives the wrong length
@@ -544,8 +544,8 @@ function run_all_trials() {
 				}
 			
 			setTimeout(function(){		
-				$('#left').hide();
-				$('#right').hide();
+				$('#left').fadeOut();
+				$('#right').fadeOut();
 			    
 
 
