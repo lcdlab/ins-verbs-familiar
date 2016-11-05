f9c40fd424104fee506fbe9e6c45bb5a72340fe3
hughrabagliati <hugh.rabagliati@gmail.com>
2016-11-05 07:43:35 -0400

    remove training, disappear foil17

--- video.js
+++ video.js
@@ -538,28 +538,34 @@ function run_all_trials() {
 				data.accuracy = 0;
 			}	
 		if (current_trial['trial_id']=='intro'){
-		
-		} else if (current_trial['trial_id']=='book_WE_feedback'){
-				if (data.accuracy == 0){
-				$(this).attr('src', 'images/dots/qu_mark.png');				
-				} else {
-			t.attr('src', 'images/dots/ElmoFace.png');
-			t.css('border', "solid 6px red");  
-			f.hide(100);
-
-			}
-			} else {
-			if (data.accuracy == 0){
-			f.attr('src', 'images/dots/ElmoFace.png');
+		if (data.accuracy == 0){
 			f.css('border', "solid 6px red");  
 			t.hide(100);			
 			}else{
-			t.attr('src', 'images/dots/ElmoFace.png');
 			t.css('border', "solid 6px red");  
 			f.hide(100);
 			}
+		
+		} else if (current_trial['trial_id']=='book_WE_feedback'){
+				if (data.accuracy == 0){
+					$(this).attr('src', 'images/dots/qu_mark.png');				
+				} else {
+					t.attr('src', 'images/dots/ElmoFace.png');
+					t.css('border', "solid 6px red");  
+					f.hide(100);
+				}
+			} else {
+				if (data.accuracy == 0){
+				f.attr('src', 'images/dots/ElmoFace.png');
+				f.css('border', "solid 6px red");  
+				t.hide(100);			
+				}else{
+				t.attr('src', 'images/dots/ElmoFace.png');
+				t.css('border', "solid 6px red");  
+				f.hide(100);
+				}
 			}
-
+		
 			
 			
 	
