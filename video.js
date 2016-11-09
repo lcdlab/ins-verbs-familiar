6a809a9a3dafdb38f9795ec1a020bf1284f89b06
hughrabagliati <hugh.rabagliati@gmail.com>
2016-11-09 07:44:08 -0500

    small subj_id

--- video.js
+++ video.js
@@ -135,7 +135,7 @@ var data = {
 };
 
 function randomString() {
-	length = 25;
+	length = 5;
     var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
